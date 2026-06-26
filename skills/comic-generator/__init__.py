3baca19c24e2fa63545c544cd6cb30c0005678a6
ClawsJoy <flyboy_qu@163.com>
2026-06-26 17:35:46 +0800

    fix: 彻底删除重复占位符skill目录

--- skills/comic-generator/__init__.py
+++
@@ -1,8 +0,0 @@
-\"\"\"comic-generator 技能\"\"\"
-
-from .comic-generator_skill import comic_generator
-
-
-def execute(params):
-    skill = comic_generator()
-    return skill.execute(params)
--- skills/sd-image-gen/__init__.py
+++
@@ -1,8 +0,0 @@
-\"\"\"sd-image-gen 技能\"\"\"
-
-from .sd-image-gen_skill import sd_image_gen
-
-
-def execute(params):
-    skill = sd_image_gen()
-    return skill.execute(params)
